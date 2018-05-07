7d98cd7ca689e96a4b4499d2fafb3d17e4a23b46
Rob Liu <therealrobophant@gmail.com>
2018-05-07 18:06:06 -0400

    Initial commit
    Initial commit

---
+++ Trig Cake.py
@@ -0,0 +1,78 @@
+import discord
+import asyncio
+from bs4 import BeautifulSoup as soup
+import urllib.request
+from datetime import datetime as dt
+
+
+client = discord.Client()
+token = 'NDM4NDI5MDYzODc5NzIwOTYw.DcEe4g.KfpWk9mlfubVU2g0TIIrmuBNUxA'
+
+# Documentation:
+# [game]u = url
+# [game] = soup object
+# [game]ud = updates list
+# [game]1r = latest update raw
+# [game]1 = latest update text
+# [game]0 = last referred update text
+# [game]h = subtitles/headers
+# [game]ht = summary of headers
+# [game]m = message in discord
+
+@client.event
+async def background_loop():
+    await client.wait_until_ready()
+    while not client.is_closed:
+
+        """Overwatch"""
+
+        # assigning variables
+        overwatchurl = 'https://playoverwatch.com/en-us/game/patch-notes/pc/'
+        with urllib.request.urlopen(overwatchurl) as doc:
+            page = doc.read()
+        overwatch = soup(page, "html.parser")
+        overwatchud = overwatch.find_all("div", class_="patch-notes-body")
+        overwatch1r = (overwatchud[0])
+        overwatch1 = overwatch1r.text
+        try:
+            overwatch0
+        except NameError:
+            overwatch0 = overwatch1
+        # message trigger
+        if overwatch1 != overwatch0:
+            overwatchh = overwatch1r.find_all("h2")
+            overwatchht = "Contents:"
+            for x in overwatchh:
+                if x.text != "PATCH HIGHLIGHTS":
+                    overwatchht += (" " + x.text + ",")
+            overwatchht = overwatchht[:-1] + "."
+            overwatchm = "Overwatch PC Patch Notes has been updated.\n" + overwatchht + "\nhttps://playoverwatch.com/en-us/game/patch-notes/pc/\n%s" % dt.today()
+            overwatch0 = overwatch1
+            await client.send_message(discord.Object(id="328295970246754304"), overwatchm)
+
+        """Fortnite"""
+
+        # assigning variables
+        fortniteurl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ'
+        with urllib.request.urlopen(fortniteurl) as doc:
+            page = doc.read()
+        fortnite = soup(page, "html.parser")
+        fortnite1r = fortnite.find("media:group")
+        fortnite1 = fortnite1r.text
+        fortniteht = fortnite1r.find("media:title").text
+        try:
+            fortnite0
+        except NameError:
+            fortnite0 = fortnite1
+        # message trigger
+        if fortnite1 != fortnite0:
+            fortnitem = "Fortnite: " + fortniteht + "\nhttps://www.youtube.com/user/epicfortnite/videos\n%s" % dt.today()
+            fortnite0 = fortnite1
+            await client.send_message(discord.Object(id="328295970246754304"), fortnitem)
+        # print("looped")
+
+        await asyncio.sleep(60*10)
+
+client.loop.create_task(background_loop())
+client.run(token)
+# /opt/vc/bin/vcgencmd measure_temp
