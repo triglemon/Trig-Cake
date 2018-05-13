429692109167448ba0fabeb26dd3b892e9e154f4
Rob Liu <therealrobophant@gmail.com>
2018-05-13 17:48:07 -0400

    Updates now stored in json
    And organized and trimmed code

--- Trig Cake.py
+++ Trig Cake.py
@@ -5,22 +5,29 @@ import discord
 import asyncio
 from bs4 import BeautifulSoup as soup
 import urllib.request
-from datetime import datetime as dt
+import json
 
 
 client = discord.Client()
 token = open("/home/pi/Desktop/token").read()
 
+
 # Documentation:
-# [game]u = url
-# [game] = soup object
+# [game]url = url
 # [game]ud = updates list
-# [game]1r = latest update raw
 # [game]1 = latest update text
 # [game]0 = last referred update text
 # [game]h = subtitles/headers
 # [game]ht = summary of headers
 # [game]m = message in discord
+
+
+def parse(url, element, attribute={}):
+    with urllib.request.urlopen(url) as doc:
+        page = doc.read()
+    object = soup(page, "html.parser")
+    return object(element, attribute)
+
 
 @client.event
 async def background_loop():
@@ -28,53 +35,55 @@ async def background_loop():
     print("All hands on deck!")
     while not client.is_closed:
 
-        """Overwatch"""
+        """Parsing"""
 
-        # assigning variables
+        # Overwatch
         overwatchurl = 'https://playoverwatch.com/en-us/game/patch-notes/pc/'
-        with urllib.request.urlopen(overwatchurl) as doc:
-            page = doc.read()
-        overwatch = soup(page, "html.parser")
-        overwatchud = overwatch.find_all("div", class_="patch-notes-body")
-        overwatch1r = (overwatchud[0])
-        overwatch1 = overwatch1r.text
-        try:
-            overwatch0
-        except NameError:
-            overwatch0 = overwatch1
-        # message trigger
+        overwatchud = parse(overwatchurl, "div", {"class": "patch-notes-body"})
+        overwatch1 = overwatchud[0].text
+        # Fortnite
+        fortniteurl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ'
+        fortniteud = parse(fortniteurl, "entry")
+        fortnite1 = fortniteud[0].find("media:title").text
+
+        """Triggers"""
+
+        # Overwatch
+        with open('/home/pi/Desktop/soupjson.json') as file:
+            soupdict = json.load(file)
+        overwatch0 = soupdict['overwatchlast']
         if overwatch1 != overwatch0:
-            overwatchh = overwatch1r.find_all("h2")
+            overwatchh = overwatchud[0].find_all("h2")
             overwatchht = "contents:"
             for x in overwatchh:
                 overwatchht += (" " + x.text.lower() + ",")
             overwatchht = overwatchht[:-1] + "."
             overwatchm = "Overwatch patch " + overwatchht + "\n<" + overwatchurl + ">"
-            overwatch0 = overwatch1
+            # Writing File
+            newdict = {"overwatchlast": overwatch1, "fortnitelast": fortnite1}
+            with open("/home/pi/Desktop/soupjson.json", "w") as file:
+                json.dump(newdict, file)
+                file.close()
             await client.send_message(discord.Object(id="328295970246754304"), overwatchm)
 
-        """Fortnite"""
-
-        # assigning variables
-        fortniteurl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ'
-        with urllib.request.urlopen(fortniteurl) as doc:
-            page = doc.read()
-        fortnite = soup(page, "html.parser")
-        fortnite1r = fortnite.find("media:group")
-        fortnite1 = fortnite1r.find("media:title").text
-        try:
-            fortnite0
-        except NameError:
-            fortnite0 = fortnite1
-        # message trigger
+        # Fortnite
+        with open('/home/pi/Desktop/soupjson.json') as file:
+            soupdict = json.load(file)
+        fortnite0 = soupdict['fortnitelast']
         if fortnite1 != fortnite0:
             fortnitem = "Fortnite: " + fortnite1 + "\n<https://www.youtube.com/user/epicfortnite/videos>"
-            fortnite0 = fortnite1
+            # Writing File
+            newdict = {"overwatchlast": overwatch1, "fortnitelast": fortnite1}
+            with open("/home/pi/Desktop/soupjson.json", "w") as file:
+                json.dump(newdict, file)
+                file.close()
             await client.send_message(discord.Object(id="328295970246754304"), fortnitem)
+
         # print("looped")
 
         await asyncio.sleep(60*10)
 
 client.loop.create_task(background_loop())
 client.run(token)
+
 # /opt/vc/bin/vcgencmd measure_temp
