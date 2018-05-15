00c1d618cb3661cb28ffc49fcc5fb9c6a7c054e2
Rob Liu <therealrobophant@gmail.com>
2018-05-15 19:41:31 -0400

    csgo update

--- Trig Cake.py
+++ Trig Cake.py
@@ -46,6 +46,7 @@ class Game:
 @client.event
 async def background_loop():
     await client.wait_until_ready()
+
     # Creating Game objects and obtaining json strings
     ow = Game("Overwatch",
               "https://playoverwatch.com/en-us/game/patch-notes/pc/",
@@ -53,16 +54,22 @@ async def background_loop():
     fn = Game("Fortnite",
               "https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ",
               "fortnitelast")
+    csgo = Game("Counter Strike: Global Offensive",
+                "http://blog.counter-strike.net/index.php/category/updates/",
+                "csgolast")
+
     print("All hands on deck!")
     # Loop code
     while not client.is_closed:
         # Parsing
         ow.parse("div", {"class": "patch-notes-body"})
         fn.parse("entry", subelement="title")
+        csgo.parse("div", {"class": "inner_post"})
 
         # Message triggers (If the last update online matches the last stored update)
 
         # Overwatch message trigger
+
         if ow.found.text != ow.last:
             headers = ow.found("h2")
             ow.message = "Overwatch:"
@@ -71,7 +78,7 @@ async def background_loop():
             ow.message = ow.message[:-1] + "."
             ow.message += "\n<" + ow.url + ">"
             # Writing File
-            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text}
+            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text, "csgolast": csgo.found.text}
             with open("/home/pi/Desktop/soupjson.json", "w") as file:
                 json.dump(newdict, file)
             ow.store()
@@ -82,12 +89,18 @@ async def background_loop():
             fn.message = fn.found.text
             fn.message = "Fortnite: " + fn.message + "\n<https://www.youtube.com/user/epicfortnite/videos>"
             # Writing File
-            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text}
+            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text, "csgolast": csgo.found.text}
             with open("/home/pi/Desktop/soupjson.json", "w") as file:
                 json.dump(newdict, file)
             fn.store()
             await client.send_message(discord.Object(id="328295970246754304"), fn.message)
 
+        if csgo.found.text != csgo.last:
+            csgo.message = "CSGO: " + csgo.found.find("h2").text
+            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text, "csgolast": csgo.found.text}
+            with open("/home/pi/Desktop/soupjson.json", "w") as file:
+                json.dump(newdict, file)
+            csgo.store()
         # print("looped")
 
         await asyncio.sleep(60*10)
