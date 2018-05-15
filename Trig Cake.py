2addf42c83e8990d58b5c4c827ad6c866726f3d8
Rob Liu <therealrobophant@gmail.com>
2018-05-14 21:57:19 -0400

    Found out what class was haha

--- Trig Cake.py
+++ Trig Cake.py
@@ -3,81 +3,90 @@
 
 import discord
 import asyncio
-from bs4 import BeautifulSoup as soup
+from bs4 import BeautifulSoup as Soup
 import urllib.request
 import json
 
-
+# Creating Discord Client and grabbing token
 client = discord.Client()
 token = open("/home/pi/Desktop/token").read()
 
 
-# Documentation:
-# [game]url = url
-# [game]ud = updates list
-# [game]1 = latest update text
-# [game]0 = last referred update text
-# [game]h = subtitles/headers
-# [game]ht = summary of headers
-# [game]m = message in discord
+class Game:
+
+    # Each game is represented by a Game object
+    def __init__(self, name, url, key):
+        self.json = "/home/pi/Desktop/soupjson.json"
+        self.name = name
+        self.url = url
+        # This grabs the json strings as the last update
+        with open(self.json) as file:
+            soupdict = json.load(file)
+        self.last = (soupdict[key])
+        self.updates = None
+        self.found = None
+        self.message = None
+
+    # This parses the url for a list of updates
+    def parse(self, element, attribute=None, subelement=None):
+        with urllib.request.urlopen(self.url) as doc:
+            page = doc.read()
+        soupobject = Soup(page, "html.parser")
+        self.updates = soupobject(element, attribute)
+        self.found = self.updates[0]
+        if subelement is not None:
+            self.found = self.found.find(subelement)
+
+    # Stores the fresh update for later batches
+    def store(self):
+        self.last = self.found.text
 
 
-def parse(url, element, attribute={}):
-    with urllib.request.urlopen(url) as doc:
-        page = doc.read()
-    object = soup(page, "html.parser")
-    return object(element, attribute)
-
-
+# This event happens at launch and is the main loop of the script
 @client.event
 async def background_loop():
     await client.wait_until_ready()
+    # Creating Game objects and obtaining json strings
+    ow = Game("Overwatch",
+              "https://playoverwatch.com/en-us/game/patch-notes/pc/",
+              "overwatchlast")
+    fn = Game("Fortnite",
+              "https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ",
+              "fortnitelast")
     print("All hands on deck!")
+    # Loop code
     while not client.is_closed:
+        # Parsing
+        ow.parse("div", {"class": "patch-notes-body"})
+        fn.parse("entry", subelement="title")
 
-        """Parsing"""
+        # Message triggers (If the last update online matches the last stored update)
 
-        # Overwatch
-        overwatchurl = 'https://playoverwatch.com/en-us/game/patch-notes/pc/'
-        overwatchud = parse(overwatchurl, "div", {"class": "patch-notes-body"})
-        overwatch1 = overwatchud[0].text
-        # Fortnite
-        fortniteurl = 'https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ'
-        fortniteud = parse(fortniteurl, "entry")
-        fortnite1 = fortniteud[0].find("media:title").text
-
-        """Triggers"""
-
-        # Overwatch
-        with open('/home/pi/Desktop/soupjson.json') as file:
-            soupdict = json.load(file)
-        overwatch0 = soupdict['overwatchlast']
-        if overwatch1 != overwatch0:
-            overwatchh = overwatchud[0].find_all("h2")
-            overwatchht = "contents:"
-            for x in overwatchh:
-                overwatchht += (" " + x.text.lower() + ",")
-            overwatchht = overwatchht[:-1] + "."
-            overwatchm = "Overwatch patch " + overwatchht + "\n<" + overwatchurl + ">"
+        # Overwatch message trigger
+        if ow.found.text != ow.last:
+            headers = ow.found("h2")
+            ow.message = "Overwatch:"
+            for x in headers:
+                ow.message += (" " + x.text.lower() + ",")
+            ow.message = ow.message[:-1] + "."
+            ow.message += "\n<" + ow.url + ">"
             # Writing File
-            newdict = {"overwatchlast": overwatch1, "fortnitelast": fortnite1}
+            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text}
             with open("/home/pi/Desktop/soupjson.json", "w") as file:
                 json.dump(newdict, file)
-                file.close()
-            await client.send_message(discord.Object(id="328295970246754304"), overwatchm)
+            ow.store()
+            await client.send_message(discord.Object(id="328295970246754304"), ow.message)
 
-        # Fortnite
-        with open('/home/pi/Desktop/soupjson.json') as file:
-            soupdict = json.load(file)
-        fortnite0 = soupdict['fortnitelast']
-        if fortnite1 != fortnite0:
-            fortnitem = "Fortnite: " + fortnite1 + "\n<https://www.youtube.com/user/epicfortnite/videos>"
+        # Fortnite message trigger
+        if fn.found.text != fn.last:
+            fn.message = fn.found.text
+            fn.message = "Fortnite: " + fn.message + "\n<https://www.youtube.com/user/epicfortnite/videos>"
             # Writing File
-            newdict = {"overwatchlast": overwatch1, "fortnitelast": fortnite1}
+            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text}
             with open("/home/pi/Desktop/soupjson.json", "w") as file:
                 json.dump(newdict, file)
-                file.close()
-            await client.send_message(discord.Object(id="328295970246754304"), fortnitem)
+            fn.store()
+            await client.send_message(discord.Object(id="328295970246754304"), fn.message)
 
         # print("looped")
 
