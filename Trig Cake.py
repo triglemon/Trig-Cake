a929e868671d842e9ec4796f261528ca18511447
Rob Liu <therealrobophant@gmail.com>
2018-05-23 22:46:04 -0400

    New branch for steam games and user options

--- Trig Cake.py
+++ Trig Cake.py
@@ -8,115 +8,75 @@ import urllib.request
 import json
 import datetime as dt
 
+
 # Creating Discord Client and grabbing token
 client = discord.Client()
-token = open("/home/pi/Desktop/token").read()
+token = open('token').read()
 
 
-class Game:
+
+class SteamApp:
 
     # Each game is represented by a Game object
-    def __init__(self, name, url, key):
-        self.json = "/home/pi/Desktop/soupjson.json"
-        self.name = name
+    def __init__(self, url):
         self.url = url
-        # This grabs the json strings as the last update
-        with open(self.json) as file:
-            soupdict = json.load(file)
-        self.last = (soupdict[key])
-        self.updates = None
+        with urllib.request.urlopen(url) as doc:
+            storepage = doc.read()
+        storesoup = Soup(storepage, 'html.parser')
+        self.name = storesoup.find('div', class_='apphub_AppName').text
+        self.id = url.split('/')[4]
+        self.nurl = 'https://store.steampowered.com/news/?appids=' + self.id
+        self.last = None
+        self.news = None
         self.found = None
-        self.message = None
 
-    # This parses the url for a list of updates
-    def parse(self, element, attribute=None, subelement=None):
-        with urllib.request.urlopen(self.url) as doc:
-            page = doc.read()
-        soupobject = Soup(page, "html.parser")
-        self.updates = soupobject(element, attribute)
-        self.found = self.updates[0]
-        if subelement is not None:
-            self.found = self.found.find(subelement)
+    def fetchjson(self, channelid):
+        with open('steam.json') as file:
+            steamdict = json.load(file)
+        self.last = (steamdict[channelid])
 
-    # Stores the fresh update for later batches
-    def store(self):
-        self.last = self.found.text
+    def parse(self):
+        with urllib.request.urlopen(self.nurl) as doc:
+            newspage = doc.read()
+        newssoup = Soup(newspage, 'html.parser')
+        self.news = newssoup('div', class_='newsPostBlock rps')
+        self.found = self.news[0].find('a').text
 
-    def log(self):
-        with open("/home/pi/Desktop/log.txt", "a") as log:
-            log.write(self.name + ", " + str(dt.datetime.now()) + "\n")
-            log.close()
+    def trigger(self):
+        pass
 
 
-# This event happens at launch and is the main loop of the script
+
 @client.event
-async def background_loop():
-    await client.wait_until_ready()
-
-    # Creating Game objects and obtaining json strings
-    ow = Game("Overwatch",
-              "https://playoverwatch.com/en-us/game/patch-notes/pc/",
-              "overwatchlast")
-    fn = Game("Fortnite",
-              "https://www.youtube.com/feeds/videos.xml?channel_id=UClG8odDC8TS6Zpqk9CGVQiQ",
-              "fortnitelast")
-    csgo = Game("Counter Strike: Global Offensive",
-                "http://blog.counter-strike.net/index.php/category/updates/",
-                "csgolast")
-
-    print("All hands on deck!")
-    # Loop code
-    while not client.is_closed:
-        # Parsing
-        ow.parse("div", {"class": "patch-notes-body"})
-        fn.parse("entry", subelement="title")
-        csgo.parse("div", {"class": "inner_post"})
-
-        # Message triggers (If the last update online matches the last stored update)
-
-        # Overwatch message trigger
-        if ow.found.text != ow.last:
-            ow.log()
-            headers = ow.found("h2")
-            ow.message = "Overwatch:"
-            for x in headers:
-                ow.message += (" " + x.text.lower() + ",")
-            ow.message = ow.message[:-1] + "."
-            ow.message += "\n<" + ow.url + ">"
-            # Writing File
-            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text, "csgolast": csgo.found.text}
-            with open("/home/pi/Desktop/soupjson.json", "w") as file:
-                json.dump(newdict, file)
-            ow.store()
-            await client.send_message(discord.Object(id="328295970246754304"), ow.message)
-
-        # Fortnite message trigger
-        if fn.found.text != fn.last:
-            fn.log()
-            fn.message = fn.found.text
-            fn.message = "Fortnite: " + fn.message + "\n<https://www.youtube.com/user/epicfortnite/videos>"
-            # Writing File
-            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text, "csgolast": csgo.found.text}
-            with open("/home/pi/Desktop/soupjson.json", "w") as file:
-                json.dump(newdict, file)
-            fn.store()
-            await client.send_message(discord.Object(id="328295970246754304"), fn.message)
-
-        if csgo.found.text != csgo.last:
-            csgo.log()
-            csgo.message = "CSGO: " + csgo.found.find("h2").text
-            csgo.message += "\nhttp://blog.counter-strike.net/index.php/category/updates/"
-            newdict = {"overwatchlast": ow.found.text, "fortnitelast": fn.found.text, "csgolast": csgo.found.text}
-            with open("/home/pi/Desktop/soupjson.json", "w") as file:
-                json.dump(newdict, file)
-            csgo.store()
-            await client.send_message(discord.Object(id="328295970246754304"), csgo.message)
-        # print("looped")
-
-        await asyncio.sleep(60 * 10)
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
 
 
-client.loop.create_task(background_loop())
+@client.event
+async def on_message(message):
+
+    if message.content.startswith('!cake'):
+        info = """**I'm tasked with checking Steam games for announcements.**
+The following commands are available at your perusal (minus brackets):
+**!add <url of store page>** — Subscribes channel to a game
+**!remove <url of store page>** — Unsubscribes channel to a game"""
+        await client.send_message(message.channel, info)
+
+    if message.content.startswith('!add'):
+        id = message.channel.id
+        url = message.content.replace('!add ', "")
+        print(url)
+        with open('steam.json', 'w+') as file:
+            dict = json.load(file)
+        if id in dict:
+            dict.update
+
+
+        await client.send_message(message.channel, 'Added')
+
+
+
 client.run(token)
-
-# /opt/vc/bin/vcgencmd measure_temp
